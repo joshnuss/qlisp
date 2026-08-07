29ba921270d52eadc353823ca3e879cf962af21a
Josh Nussbaum <joshnuss@gmail.com>
2026-08-06 20:49:09 -0400

    Refactors entry point

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { exec } from './exec.ts'
 import * as repl from './repl.ts'
 
-if (process.argv.length > 2) {
-  const filePath = process.argv[2]
+const filePath = process.argv[2]
 
-  if (!filePath) throw new Error('missing file path')
-
+if (filePath) {
   await exec(filePath)
 } else {
   repl.start()
