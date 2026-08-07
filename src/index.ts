e910246207123d8a760cc9a3e2e2c80879734cb9
Josh Nussbaum <joshnuss@gmail.com>
2026-08-06 20:23:31 -0400

    Adds eval to cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('+')
+import { evalFile } from './interpreter.ts'
+
+if (process.argv.length > 2) {
+  const filePath = process.argv[2]
+
+  if (!filePath) throw new Error('missing file path')
+
+  const result = await evalFile(filePath)
+
+  if ('value' in result) {
+    console.log(result.value)
+  }
+} else {
+  await import('./repl.ts')
+}
